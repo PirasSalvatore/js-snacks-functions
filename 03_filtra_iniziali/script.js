b233b98ce361f02526cdc30e37436b0e8b584fa7
PirasSalvatore <torep.92@gmail.com>
2025-02-17 15:38:11 +0100

    add snaks 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -5,9 +5,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+const firstCharWord = (arr, a) => {
+    const arrayFirstCharWord = []
+    for (let i = 0; i < arr.length; i++) {
+        if (((arr[i].toLowerCase()).split(""))[0] === a) {
+            arrayFirstCharWord.push(arr[i])
+        }
+    }
+    return arrayFirstCharWord
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(firstCharWord(names, "a"));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
