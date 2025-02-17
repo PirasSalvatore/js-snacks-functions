e65ef7264fdb67227fb20a37c2615e29923181a5
PirasSalvatore <torep.92@gmail.com>
2025-02-17 16:04:09 +0100

    add snaks 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,22 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = 'javascript'
 
 // Dichiara la funzione qui.
 
+const countVocal = str => {
+    let cont = 0
+    const arrayStr = str.toLowerCase().split("")
+    for (let i = 0; i < arrayStr.length; i++) {
+        if (arrayStr[i] === "a" || arrayStr[i] === "e" || arrayStr[i] === "i" || arrayStr[i] === "o" || arrayStr[i] === "u") {
+            cont++
+        }
+    }
+    return cont
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(countVocal(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
