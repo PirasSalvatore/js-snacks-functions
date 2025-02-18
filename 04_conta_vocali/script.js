1f1e471abf6544e2b1d88561bb692408de30c4c1
PirasSalvatore <torep.92@gmail.com>
2025-02-18 10:31:44 +0100

    Add alternative function of snack 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -13,11 +13,37 @@ const word = 'javascript'
  * @param {Array} str parola o frase
  * @returns {Number} cont Numero vocali parola
  */
-const countVocal = str => {
+/* const countVocal = str => {
     let cont = 0
+    const vowels = ['a', 'e', 'i', 'o', 'u']
     const arrayStr = str.toLowerCase().split("")
     for (let i = 0; i < arrayStr.length; i++) {
         if (arrayStr[i] === "a" || arrayStr[i] === "e" || arrayStr[i] === "i" || arrayStr[i] === "o" || arrayStr[i] === "u") {
+            cont++
+        }
+    }
+    return cont
+}
+ */
+/**
+ * presa una stringa all'ingresso
+ * creiamo un contatore inizializato a 0
+ * creo un array contenente tutte le vocali
+ * trsformiamo la stringa in un array scritto tutto in minuscolo
+ * confrontiamo ogni carattere del nuovo array creato se è incluso nell'array delle vocali
+ *      se il carattere è incluso allora si aggiunge 1 al contatore
+ * restituisce il contatore delle vocali
+ * @param {Array} str parola o frase
+ * @returns {Number} cont Numero vocali parola
+ */
+const countVocal = str => {
+    let cont = 0
+    const vowels = ['a', 'e', 'i', 'o', 'u']
+    const arrayStr = str.toLowerCase().split("")
+
+    for (let i = 0; i < arrayStr.length; i++) {
+        const thisChar = arrayStr[i]
+        if (vowels.includes(thisChar)) {
             cont++
         }
     }
